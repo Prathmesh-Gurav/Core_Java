09a3604ca45183fdc4c27b73f4403f5d0e28c8bb
Shree <Shree@DESKTOP-E3CCJVB>
2023-08-30 19:17:27 +0530

    commit

---
+++ Shruti_Maam_Code/src/Day_7_WhileDoWhile/DoWhileDemo.java
@@ -0,0 +1,14 @@
+package Day_7_WhileDoWhile;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		int i=13;
+		do
+		{
+			System.out.println(i+"Hello");
+			i++;
+		}while(i<=10);
+	}
+
+}
